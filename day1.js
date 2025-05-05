7bb4b717a263ac61194dc4d73765246b6a7e7871
Akshata R Bhat <akshata.bhat@probeinformation.com>
2025-05-05 12:41:20 +0530

    day 1

--- day1.js
+++ day1.js
@@ -1,27 +1,24 @@
-function maximizeProfit(prices) {
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
     if(!prices.length) return;
 
     let lowestPrice = prices[0];
-    let lowestPriceDay = 0
+    let profit = 0;
     for(let i=0; i< prices.length; i++) {
-        if(lowestPrice >= prices[i]) {
-            lowestPrice = prices[i];    
-            lowestPriceDay = i;
-        }        
+        if(prices[i] <= lowestPrice) lowestPrice = prices[i];
+
+        if(prices[i] - lowestPrice > profit) profit = prices[i] - lowestPrice;
+     
     }
-    let highestPricesArray = prices.slice(lowestPriceDay);
-    let highestPrice = highestPricesArray[0];
-    if(highestPricesArray.length) {
-        for(let i=0; i< highestPricesArray.length; i++) {
-            if(highestPrice <= highestPricesArray[i]) highestPrice = highestPricesArray[i];
-        }
-        return highestPrice-lowestPrice;
-    }
-    else return 0
+    return profit;
 
+};
 
-}
+console.log(maximizeProfit([2,1,2,1,0,0,1]))
 
-console.log(maximizeProfit([7,1,5,3,6,4]))
-console.log(maximizeProfit([7,6,4,3,1]))
-console.log(maximizeProfit([5,3,1,4,2,10,5,1,11]))
+// console.log(maximizeProfit([7,1,5,3,6,4]))
+// console.log(maximizeProfit([7,6,4,3,1]))
+// console.log(maximizeProfit([5,3,1,4,2,10,5,1,11]))
